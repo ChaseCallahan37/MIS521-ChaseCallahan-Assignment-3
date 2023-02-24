82d19654e098316b88572ead2271ce9859fc97f6
ChaseCallahan37 <ctcallahan2@crimson.ua.edu>
2023-02-23 20:35:47 -0600

    pre scaffold

---
+++ Models/ActorMovie.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment_3.Models
+{
+    public class ActorMovie
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Actor")]
+        public int ActorId { get; set; }
+        [Required]
+        public Actor Actor { get; set; }
+        [ForeignKey("Movie")]
+        public int MovieId { get; set; }
+        [Required]
+        public Movie Movie { get; set; }
+    }
+}
