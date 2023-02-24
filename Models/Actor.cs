e06bb427be455eb56468804c4d6dff5d951c8298
ChaseCallahan37 <ctcallahan2@crimson.ua.edu>
2023-02-23 20:08:06 -0600

    Add project files.

---
+++ Models/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_3.Models
+{
+    public class Actor
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string IMBDLink { get; set; }
+
+        public byte[]? Image { get; set; }
+    }
+}
